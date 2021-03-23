c09e64625630731950b3342a6ae6b256fb04e9e8
Vladimir Murashka <Murashka.Vladimir@yandex.ru>
2021-03-23 13:35:13 +0300

    Work with logic.

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLable: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLable.isHidden = true
+        showTextButton.layer.cornerRadius = 10
+        
+        
     }
 
-
+    @IBAction func showButtonPressed() {
+        helloWorldLable.isHidden.toggle()
+        if helloWorldLable.isHidden {
+            showTextButton.setTitle("Show Text", for: .normal)
+        } else {
+            showTextButton.setTitle("Hide Text", for: .normal)
+        }
+    }
+    
 }
 
